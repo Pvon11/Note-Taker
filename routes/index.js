0e758bf381ac68671ed106428d81e651c2c78b14
Pvon11 <patrickvonatbone@gmail.com>
2023-03-13 18:09:24 -0400

    fixed pathing

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db/db.json");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+router.get("/api/notes", (req, res) => {
+  const data = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf-8")
+  );
+
+  res.status(200).json(data);
+});
+
+router.post("/api/notes", (req, res) => {
+  console.log(uuidv4());
+  const postData = { ...req.body, id: uuidv4() };
+
+  const savedData = db;
+  savedData.push(postData);
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(savedData)
+  );
+  res.status(200).json(savedData);
+});
+
+//   let dataFromDb = JSON.parse(
+//     fs.readFileSync(__dirname + "/db/db.json", "utf-8")
+//   );
+
+//   dataFromDb.push(postData);
+
+//   fs.writeFileSync(
+//     __dirname + "/db/db.json",
+//     JSON.stringify(dataFromDb),
+//     "utf-8"
+//   );
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,52 @@
 const express = require("express");
 const path = require("path");
-const database = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
+const db = require("./db/db.json");
+const routes = require("./routes");
 const PORT = 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
+app.use(routes);
 
 // GET Route for notes
+// app.get("/notes", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/public/notes.html"))
+// );
+
+// app.get("/api/notes", (req, res) => {
+//   const data = JSON.parse(fs.readFileSync(__dirname + "/db/db.json", "utf-8"));
+
+//   res.status(200).json(data);
+// });
+
+// app.post("/api/notes", (req, res) => {
+//   const postData = { ...req.body, id: uuidv4() };
+
+//   let dataFromDb = JSON.parse(
+//     fs.readFileSync(__dirname + "/db/db.json", "utf-8")
+//   );
+
+//   dataFromDb.push(postData);
+
+//   fs.writeFileSync(
+//     __dirname + "/db/db.json",
+//     JSON.stringify(dataFromDb),
+//     "utf-8"
+//   );
+
+//   res.status(200).json(dataFromDb);
+// });
+
+// GET Route for index page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
-
-// GET Route for index page
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
